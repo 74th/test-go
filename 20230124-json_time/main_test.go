13bf258725e5263bfa8aea341edfc7487fc65000
Atsushi Morimoto (74th) <74th.tech@gmail.com>
2024-01-24 19:12:43 +0900

    up from

--- 20230124-json_time/main_test.go
+++ 20230124-json_time/main_test.go
@@ -21,22 +21,42 @@ func TestJsonTime(t *testing.T) {
 	b, err := json.Marshal(d)
 	fmt.Println("ERR:", err)
 	fmt.Println("DATA:", string(b))
-	// ERR: <nil>
-	// DATA: {"t":"2024-01-24T15:54:09.458099807+09:00"}
 
 	// time.Time は JSON になると、RFC3339 になる
+	// DATA: {"t":"2024-01-24T15:54:09.458099807+09:00"}
 
 	d = Data{}
 	b, _ = json.Marshal(d)
 	fmt.Println("DATA:", string(b))
 
-	// DATA: {"t":"0001-01-01T00:00:00Z"}
-
 	// time.Time は omitempty に対応していない
+	// DATA: {"t":"0001-01-01T00:00:00Z"}
 
 	now := time.Now()
 	d2 := Data2{Time: &now}
 	b, _ = json.Marshal(d2)
 	fmt.Println("DATA:", string(b))
 
+	d2 = Data2{}
+	b, _ = json.Marshal(d2)
+	fmt.Println("DATA:", string(b))
+
+	// ポインタにするとnullに対応する
+	// DATA: {"t":null}
+
+	d = Data{}
+	err = json.Unmarshal(b, &d)
+	fmt.Println("ERR:", err)
+	fmt.Printf("DATA: %#v\n", d)
+
+	// null はゼロ値になる
+	// DATA: main.Data{Time:time.Date(1, time.January, 1, 0, 0, 0, 0, time.UTC)}
+
+	d = Data{}
+	err = json.Unmarshal([]byte("{}"), &d)
+	fmt.Println("ERR:", err)
+	fmt.Printf("DATA: %#v\n", d)
+
+	// プロパティがなくてもゼロ値になる
+	// DATA: main.Data{Time:time.Date(1, time.January, 1, 0, 0, 0, 0, time.UTC)}
 }
