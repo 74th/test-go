0bb23a7df4de3025d13d1f88ee1a1dd4d900c213
Atsushi Morimoto (74th) <74th.tech@gmail.com>
2024-01-24 16:09:50 +0900

    up from

--- 20230124-json_time/main_test.go
+++ 20230124-json_time/main_test.go
@@ -7,12 +7,16 @@ import (
 	"time"
 )
 
-type D struct {
-	T time.Time `json:"t"`
+type Data struct {
+	Time time.Time `json:"t",omitempty`
+}
+
+type Data2 struct {
+	Time *time.Time `json:"t",omitempty`
 }
 
 func TestJsonTime(t *testing.T) {
-	d := D{T: time.Now()}
+	d := Data{Time: time.Now()}
 
 	b, err := json.Marshal(d)
 	fmt.Println("ERR:", err)
@@ -21,4 +25,18 @@ func TestJsonTime(t *testing.T) {
 	// DATA: {"t":"2024-01-24T15:54:09.458099807+09:00"}
 
 	// time.Time は JSON になると、RFC3339 になる
+
+	d = Data{}
+	b, _ = json.Marshal(d)
+	fmt.Println("DATA:", string(b))
+
+	// DATA: {"t":"0001-01-01T00:00:00Z"}
+
+	// time.Time は omitempty に対応していない
+
+	now := time.Now()
+	d2 := Data2{Time: &now}
+	b, _ = json.Marshal(d2)
+	fmt.Println("DATA:", string(b))
+
 }
