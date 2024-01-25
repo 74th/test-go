28197d4e7339e97b759dffe9c0680446eefb3649
Atsushi Morimoto (74th) <74th.tech@gmail.com>
2024-01-25 14:50:59 +0900

    update

--- new.sh
+++ new.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+set -xe
 
 if [ $# -ne 1 ]; then
     echo "Usage: $0 <new_directory_name>"
     exit 1
 fi
 
-mkdir $1
+mkdir -p $1
+cp template/main_test.go ./$1/
 cd $1
 go mod init github.com/74th/go-testing/$1
+cd ..
 go work use $1
