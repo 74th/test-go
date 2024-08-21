b3f0d3b0f26a04f48f260d634ad669f60b843740
Atsushi Morimoto (74th) <74th.tech@gmail.com>
2024-08-21 08:18:31 +0000

    update

---
+++ 20240821-slices_sort/go.mod
@@ -0,0 +1,3 @@
+module github.com/74th/testing-go/20240821-slices_sort
+
+go 1.22.5
---
+++ 20240821-slices_sort/main_test.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"log"
+	"slices"
+	"sort"
+	"testing"
+)
+
+type Record struct {
+	ID   int
+	Name string
+}
+
+func TestSlices(t *testing.T) {
+	rs := []Record{
+		{ID: 20, Name: "B"},
+		{ID: 10, Name: "A"},
+		{ID: 30, Name: "C"},
+	}
+
+	slices.SortFunc(rs, func(a, b Record) int {
+		return a.ID - b.ID
+	})
+
+	log.Printf("slices.SortFunc() rs: %v", rs)
+
+	target := 20
+	n, ok := slices.BinarySearchFunc(rs, Record{ID: target}, func(a, b Record) int {
+		return a.ID - b.ID
+	})
+	log.Printf("slices.BinarySearchFunc() target: %d, n: %d, ok: %v", target, n, ok)
+
+	target = 15
+	n, ok = slices.BinarySearchFunc(rs, Record{ID: target}, func(a, b Record) int {
+		return a.ID - b.ID
+	})
+	log.Printf("slices.BinarySearchFunc() target: %d, n: %d, ok: %v", target, n, ok)
+
+}
+
+func TestSort(t *testing.T) {
+	rs := []Record{
+		{ID: 20, Name: "B"},
+		{ID: 10, Name: "A"},
+		{ID: 30, Name: "C"},
+	}
+
+	sort.Slice(rs, func(i, j int) bool {
+		return rs[i].ID < rs[j].ID
+	})
+
+	log.Printf("sort.Find() rs: %v", rs)
+
+	target := 20
+	n, ok := sort.Find(len(rs), func(i int) int {
+		return target - rs[i].ID
+	})
+	log.Printf("sort.Find() target: %d, n: %d, ok: %v", target, n, ok)
+
+	target = 15
+	n, ok = sort.Find(len(rs), func(i int) int {
+		return rs[i].ID - target
+	})
+	log.Printf("sort.Find() target: %d, n: %d, ok: %v", target, n, ok)
+}
